d3ba434748cf610b1005f756999f0c1b6b19a352
akshata_rn <akshatarn123@gamail.com>
2023-01-05 11:14:24 +0530

    Creating Table

--- payroll.sql
+++ payroll.sql
@@ -1,2 +1,12 @@
 ---Creating database---
 CREATE DATABASE PayrollService;
+
+---Creating Table---
+CREATE TABLE EmployeePayroll
+(
+ID INT IDENTITY(1,1) PRIMARY KEY,
+NAME VARCHAR(200),
+SALARY FLOAT,
+STARTDATE DATE
+);
+
