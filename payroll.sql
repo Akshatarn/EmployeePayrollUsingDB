81b4898c0a810262d836df5d41fee374a61bb6be
akshata_rn <akshatarn123@gamail.com>
2023-01-05 11:24:25 +0530

    Getting all the values from table

--- payroll.sql
+++ payroll.sql
@@ -10,9 +10,12 @@ SALARY FLOAT,
 STARTDATE DATE
 );
 
+---Inserting Values into table---
 INSERT INTO EmployeePayroll VALUES('Vijay',25000,'2020-03-04'),
 ('Rohit',80000,'2020-07-06'),
 ('Geetha',74000,'2020-05-26'),
 ('Rohit',80000,'2020-02-05'),
 ('Rohit',80000,'2021-08-13');
+
+---Geting data from table---
 SELECT *FROM EmployeePayroll;
