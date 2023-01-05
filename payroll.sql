d20a4de417353c5174d5e16a0ea90e96819955b1
akshata_rn <akshatarn123@gamail.com>
2023-01-05 11:21:41 +0530

    Adding values to the table

--- payroll.sql
+++ payroll.sql
@@ -10,3 +10,9 @@ SALARY FLOAT,
 STARTDATE DATE
 );
 
+INSERT INTO EmployeePayroll VALUES('Vijay',25000,'2020-03-04'),
+('Rohit',80000,'2020-07-06'),
+('Geetha',74000,'2020-05-26'),
+('Rohit',80000,'2020-02-05'),
+('Rohit',80000,'2021-08-13');
+SELECT *FROM EmployeePayroll;
